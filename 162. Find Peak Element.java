d294bec3f07e63288f5b4e28156d5e0cb51986f7
Abdul Rahman Anas A <144148388+AbdulRahmanAnasA@users.noreply.github.com>
2024-08-30 19:26:03 +0530

    Update 162. Find Peak Element.java

--- 162. Find Peak Element.java
+++ 162. Find Peak Element.java
@@ -23,3 +23,29 @@ class Solution {
        return -1;
     }
 }
+
+//optimal
+
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int n=arr.length;
+       
+      
+
+       int low=1;
+       int high=n-2;
+       while(low<=high){
+        int mid=(low+high)/2;
+
+        
+        if(arr[mid]<arr[mid+1]){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    } 
+    return low;
+
+    }
+}
